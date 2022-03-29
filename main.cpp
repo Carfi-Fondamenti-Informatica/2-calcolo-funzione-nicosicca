b5d7479087b74635fe2eed5495f75992fc12a1d9
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-03-29 11:16:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   
+   double a,b,x,y,z;
+   cin>>a>>b>>x>>y;
+   
+   if(x<0)&&(y>0))
+      z=a*x-b*y;
+   else
+      {
+         if((x>=0)&&(y<=0))
+         {
+            z=a*x*x-b*y;
+         }
+         else
+            z=a*x+b*y*y;
+      }
+   
+   cout<<z;
+   
    return 0;
 }
